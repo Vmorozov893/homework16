92ef8a3073841bd1fd5897eb39c4b4d6f4040e7a
vmorozov893 <vmorozov893@gmail.com>
2024-10-06 01:43:18 +0300

    Точно готовое решение.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import exeption.WrongLoginException;
+import exeption.WrongPasswordException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,7 +8,15 @@ public class Main {
         String login = "vladik_221";
         String password = "password";
         String confirmPassword = "pasword";
-        Validator.validate(login,password,confirmPassword);
-        System.out.println("Валидация прошла успешно!");
+
+        try {Validator.validate(login,password,confirmPassword);
+        } catch (WrongLoginException wrongLoginException){
+            System.out.println("Произошла ошибка при проверке логина: " + wrongLoginException.getMessage());
+        } catch (WrongPasswordException wrongPasswordException){
+            System.out.println("Произошла ошибка при проверке пароля: " + wrongPasswordException.getMessage());
+        } finally {
+            System.out.println("Валидация завершена!");
+        }
+
     }
 }

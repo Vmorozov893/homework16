7250053c3e3a18d5f37a769d3884edea3bd31adf
vmorozov893 <vmorozov893@gmail.com>
2024-10-06 01:23:15 +0300

    Готовое решение.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
+        String login = "vladik_221";
+        String password = "password";
+        String confirmPassword = "pasword";
+        Validator.validate(login,password,confirmPassword);
+        System.out.println("Валидация прошла успешно!");
     }
 }
